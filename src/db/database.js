9b1f55937d09dc422e9f41b8310a2d6f51a2cc77
casino-tango <unicasinosdecombiasa@gmail.com>
2024-10-21 15:12:56 -0500

    conexion ala db

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,18 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequelize('prueba12', 'postgres', 'root', {
-    host: 'localhost',
-    dialect:"postgres",
-  });
+
+
+
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: true,
+  },
+  logging: false, 
+});
